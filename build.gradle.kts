2a68efce3ea386371a840b0976fea86726168c8b
Viktoriia Zubkova <vichgirl@gmail.com>
2025-06-29 19:43:41 +0300

    feat: add configuration file, User

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.sonarqube") version "6.2.0.5505"
+	id("io.freefair.lombok") version "8.6"
 }
 
 group = "hexlet.code"
@@ -20,8 +21,15 @@ repositories {
 }
 
 dependencies {
+	runtimeOnly("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("net.datafaker:datafaker:2.0.2")
+	implementation("org.instancio:instancio-junit:3.3.1")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,45 @@
+package hexlet.code.model;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @CreatedDate
+    private LocalDate updatedAt;
+}
