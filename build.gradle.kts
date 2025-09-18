31e76b137f55cba876373adc497a4babf593579a
Viktoriia Zubkova <vichgirl@gmail.com>
2025-09-18 17:32:09 +0300

    added dependency 'postgresql'

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	runtimeOnly("com.h2database:h2")
+	implementation("org.postgresql:postgresql")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
