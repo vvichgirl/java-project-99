8888bfbe2ad037f9cc7721c373aafbaf236ae458
Viktoriia Zubkova <vichgirl@gmail.com>
2025-09-22 18:46:00 +0300

    UserUtils: unnecessary methods removed

--- src/main/java/hexlet/code/utils/UserUtils.java
+++ src/main/java/hexlet/code/utils/UserUtils.java
@@ -1,6 +1,5 @@
 package hexlet.code.utils;
 
-import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,23 +10,9 @@ public class UserUtils {
     @Autowired
     private UserRepository userRepository;
 
-    public User getCurrentUser() {
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
-        }
-        var email = authentication.getName();
-        return userRepository.findByEmail(email).get();
-    }
-
     public boolean isOwner(long userId) {
         String ownerEmail = userRepository.findById(userId).get().getEmail();
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         return ownerEmail.equals(authentication.getName());
-    }
-
-    public User getTestUser() {
-        return  userRepository.findByEmail("hexlet@example.com")
-                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
