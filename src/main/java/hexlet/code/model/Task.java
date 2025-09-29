ffd07dfa5c1c92fa37e6764184717c28becfd7c6
Viktoriia Zubkova <vichgirl@gmail.com>
2025-09-29 17:15:13 +0300

    add JoinTable

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -51,6 +53,11 @@ public class Task {
     private User assignee;
 
     @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "tasks_labels",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "label_id")
+    )
     private Set<Label> labels = new HashSet<>();
 
     @CreatedDate
